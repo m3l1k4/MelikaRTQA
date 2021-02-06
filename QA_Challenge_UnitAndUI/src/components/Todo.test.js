b8f94f0e3cbd71f827e3585c9d2832b16007034a
m3l1k4 <melikaihelas@gmail.com>
2021-02-05 23:03:34 -0800

    check strikethrough test for todo.js

--- QA_Challenge_UnitAndUI/src/components/Todo.test.js
+++ QA_Challenge_UnitAndUI/src/components/Todo.test.js
@@ -1,23 +1,16 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { getQueriesForElement } from "@testing-library/dom";
-import {render, fireEvent, waitFor, screen} from "@testing-library/react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import { toHaveStyle } from '@testing-library/jest-dom'
 import { App } from "./App";
 import Todo from "./Todo"
-import {TodoList} from "./TodoList"
+import { TodoList } from "./TodoList"
 
-
-
-const Button = ({ onClick, children }) => (
-  <button onClick={onClick}>{children}</button>
-)
-
-test('check strikethrough on click for list items', async() => {
-
-const handleClick = jest.fn();
-const textsample = "potato";
-const compstatus = true;
-const{getByText}=render(<Todo onClick={handleClick} completed={compstatus} text={textsample} ></Todo>)
-expect(getByText("potato")).toHaveStyle('textDecoration: line-through')
+test('check strikethrough on click for list items', () => {
+  const handleClick = jest.fn();
+  const textsample = "potato";
+  const compstatus = true;
+  const { getByText } = render(<Todo onClick={handleClick} completed={compstatus} text={textsample} ></Todo>)
+  expect(getByText("potato")).toHaveStyle('textDecoration: line-through')
 })
