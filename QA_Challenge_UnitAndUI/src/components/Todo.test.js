3fba3a957deed0c39bc8a6b0a4936d9632d7dec6
m3l1k4 <melikaihelas@gmail.com>
2021-02-07 19:27:26 -0800

    comments added

--- QA_Challenge_UnitAndUI/src/components/Todo.test.js
+++ QA_Challenge_UnitAndUI/src/components/Todo.test.js
@@ -9,10 +9,29 @@ import { TodoList } from "./TodoList"
 
 //this component striks through text when clicked on
 
+//using test, tests one time for the text decoration style in event of complete status
+// this is short and requires few lines, but it is not testing for the case of text decoration being "none"
 test('check strikethrough on click for list items', () => {
   const handleClick = jest.fn();
   const textsample = "potato";
   const compstatus = true;
   const { getByText } = render(<Todo onClick={handleClick} completed={compstatus} text={textsample} ></Todo>)
   expect(getByText("potato")).toHaveStyle('textDecoration: line-through')
+})
+
+//using describe, performs both complete and not complete status tests
+// im pretty sure there is a better way to write this so we're not repeating the render statement
+describe("todo strikethrough", () => {
+  const handleClick = jest.fn();
+  const textsample = "potato";
+  const compstatus = false;
+  it("tasks should not be striked by default", () => {
+    const { getByText } = render(<Todo onClick={handleClick} completed={compstatus} text={textsample} ></Todo>)
+    expect(getByText(textsample)).toHaveStyle('textDecoration: none')
+  });
+  it("text strikethrough on click", () =>{
+    const { getByText } = render(<Todo onClick={handleClick} completed={!compstatus} text={textsample} ></Todo>)
+    expect(getByText(textsample)).toHaveStyle('textDecoration: line-through')
+  });
+  
 })
