44874a634e0579e98fa3238324a2629def19602b
m3l1k4 <melikaihelas@gmail.com>
2021-02-08 20:09:46 -0800

    test user add to list

--- QA_Challenge_UnitAndUI/cypress/integration/todoappui.spec.js
+++ QA_Challenge_UnitAndUI/cypress/integration/todoappui.spec.js
@@ -7,4 +7,6 @@ it('should', ()=>{
     cy.visit('/')
     // cy.get('.App-link')//select by class
     // cy.contains('Learn React')
+    cy.get('input').type('Hello, World')
+    cy.get('form').submit()
 })
